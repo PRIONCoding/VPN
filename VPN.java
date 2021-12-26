bdc90d03f5a594112a13bd24b5a59d3e533f082e
FERDdeveloper <86313430+ferderplays@users.noreply.github.com>
2021-12-26 16:47:39 +0100

    1.0

---
+++ VPN.java
@@ -0,0 +1,6 @@
+public class VPN {
+    public void PrionVPN() {
+        // this is supposed to change your IP to something else
+        String setIP = System.setProperty("user.ip", "127.67.94.13");
+    }
+}
